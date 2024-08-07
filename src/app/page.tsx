83d8b716d92ef71c441563e0c1bb7c711369aece
Felipe Rodriguez <jrovez@outlook.com>
2024-08-07 07:19:10 -0500

    css modules deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client"
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1> Hola Mundo</h1>
     </main>
   );
