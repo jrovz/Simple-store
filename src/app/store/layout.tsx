c66a35e86ef23a4ef71f75285ff5b3b09c93f62b
Felipe Rodriguez <jrovez@outlook.com>
2024-08-08 08:41:34 -0500

    update

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,3 +1,6 @@
+import { Inter } from "next/font/google"
+import { Header } from 'app/components/shared/Header'
+
 export default function layout({ children }: {children: React.ReactNode}){
     return(
         <main>
