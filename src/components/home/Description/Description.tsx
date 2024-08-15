ae7cd19f233691b51d69bdc88b8c83758544fb6e
jrovz <jrovz@Hyprland.Home>
2024-08-15 14:25:25 -0500

    Actualizaciones al home, y cambio de diseño del header

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,21 +1,23 @@
+import { styleText } from "util"
+import styles from './Description.module.sass'
+
 export const Description = () => {
     return(
         
-        <section className="Main-section">
+        <section>
             <article>
-            <div className="info-section">
-                <h1>Descripcion</h1>
+            <div className={styles.Description}>
+                <img src="/images/img1.jpeg"/>
+                <h2>SIMPLE</h2>
                 <p>
-                    SIMPLE una tienda pensada en la comodidad de ver y sentirse bien. Es un concepto inspirado
+                    una tienda pensada en la comodidad de ver y sentirse bien. Es un concepto inspirado
                     en lo sobrio y lo versatil, tiene como proposito deconstruir el concepto que tenemos al rededor
                     de la "moda", porque el toque mas importante siempre lo da nuestra actitud.
                 </p>
                 <button>Conoce más</button>
             </div>
             
-            <div className="image-section">
-                <img src="" alt=""></img>
-            </div>
+            
             </article>
             
         </section>
