f7a19403544d397ef09219bb95bb8869206d2ed5
juan Felipe Rodriguez Valencia <jrovez@outlook.com>
2024-08-26 16:18:02 -0500

    actualizaciones generales main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,7 @@ export default function Home() {
       <Description />
       
       
-      <Gallery />
-      
-      <Footer />
+        
 
       
     </main>
--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,6 +1,32 @@
+import Image from "next/image"
+import styles from './Footer.module.sass'
+
+
 export const Footer = () => {
     return(
         <footer>
+            <div className={styles.Footer}>
+                <div className={styles.Footer__social}>
+                    
+                    <h1>
+                        contactanos
+                    </h1>
+                    <p>
+                        Instagram : @simple_tienda
+                    </p>
+                    
+                </div>
+                <div className={styles.Footer__logo}>
+                    <Image 
+                        src="/images/insta.png" 
+                        alt="" 
+                        fill 
+                        
+                    />
+                </div>
+                
+            </div>
+            
             <p> Simple Store </p>
         </footer>
     )
